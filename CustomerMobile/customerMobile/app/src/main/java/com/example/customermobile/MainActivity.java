a8c731b0f4f76a9916db5b02d362dbf317e648ac
puppyfoot <kjhkjh5265@smail.kongju.ac.kr>
2020-11-26 23:40:37 +0900

    Update CustomerMobile 1. 차량 조회 및 제어화면 추가 - home.xml 추가

--- CustomerMobile/customerMobile/app/src/main/java/com/example/customermobile/MainActivity.java
+++ CustomerMobile/customerMobile/app/src/main/java/com/example/customermobile/MainActivity.java
@@ -22,35 +22,35 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home);
 
-        circularProgressView = findViewById(R.id.circularProgressView);
-        circularProgressView.setProgress(0);
-        circularProgressView.setMaxValue(100);
-
-        progressView = findViewById(R.id.progressView);
-        progressView.setOnProgressChangeListener(new OnProgressChangeListener() {
-            @Override
-            public void onChange(float v) {
-                progressView.setLabelText("Progress: " + v + "%");
-            }
-        });
+//        circularProgressView = findViewById(R.id.circularProgressView);
+//        circularProgressView.setProgress(0);
+//        circularProgressView.setMaxValue(100);
+//
+//        progressView = findViewById(R.id.progressView);
+//        progressView.setOnProgressChangeListener(new OnProgressChangeListener() {
+//            @Override
+//            public void onChange(float v) {
+//                progressView.setLabelText("Progress: " + v + "%");
+//            }
+//        });
     }
 
     public void clickbt(View v){
-        if(v.getId() == R.id.button1){
-            circularProgressView.setProgress(circularProgressView.getProgress() + 5);
-        }else if(v.getId() == R.id.button2){
-            MotionToast.Companion.createToast(this,
-                    "Hurray success 😍",
-                    "Upload Completed successfully!",
-                    MotionToast.TOAST_SUCCESS,
-                    MotionToast.GRAVITY_BOTTOM,
-                    MotionToast.LONG_DURATION,
-                    ResourcesCompat.getFont(this,R.font.helvetica_regular));
-        }else if(v.getId() == R.id.button3){
-            Random r = new Random();
-            progressView.setProgress(r.nextInt(100));
-        }
+//        if(v.getId() == R.id.button1){
+//            circularProgressView.setProgress(circularProgressView.getProgress() + 5);
+//        }else if(v.getId() == R.id.button2){
+//            MotionToast.Companion.createToast(this,
+//                    "Hurray success 😍",
+//                    "Upload Completed successfully!",
+//                    MotionToast.TOAST_SUCCESS,
+//                    MotionToast.GRAVITY_BOTTOM,
+//                    MotionToast.LONG_DURATION,
+//                    ResourcesCompat.getFont(this,R.font.helvetica_regular));
+//        }else if(v.getId() == R.id.button3){
+//            Random r = new Random();
+//            progressView.setProgress(r.nextInt(100));
+//        }
     }
 }
